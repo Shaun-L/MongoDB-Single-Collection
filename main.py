f3c43c404a64dd8a2a39aaf51fb6f17f528c2608
Shaun Lim <shaunlim1214@gmail.com>
2023-12-11 10:57:27 -0800

    late final commit

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
-# This is a sample Python script.
-
+from pymongo import MongoClient
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    cluster = "mongodb+srv://shaunlim:<Cathrina1976>@cecs-323-fall.44qveqy.mongodb.net/?retryWrites=true&w=majority"
+    client = MongoClient(cluster)
+    db = client.shaundb
+
+
+
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
